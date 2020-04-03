4c3ed6cc5d7448ce37bfb1a68504d8f3e5fcc57d
carmine <carminelentisco@gmail.com>
2020-04-03 10:37:36 +0200

    JS Palindromo
    Riaggunta della versione base + versione migliorata

--- JS/palindroma.js
+++ JS/palindroma.js
@@ -1,10 +1,44 @@
-/********************************************************************
+/********************************************************************\
  * 
  * Palidroma
  * Chiedere all’utente di inserire una parola
  * Creare una funzione per capire se la parola inserita è palindroma
  * 
- *******************************************************************/
+\ *******************************************************************/
+
+/************************** INIZIO FORMA BASE ***********************/
+
+/** // Chiedere all’utente di inserire una parola
+ * 
+ * var parolaUtente = prompt('Inserisci una parola : ');
+ * 
+ * // Richiamo della funzione
+ * 
+ * var risultato = palindroma(parolaUtente);
+ * console.log('La parola ' + parolaUtente + ', da te inserita, ' + risultato);
+ * 
+ * // Funzione palindroma
+ * 
+ * function palindroma (parola) {
+ *      
+ *    var reverse = "";
+ * 
+ * 
+ *    for (var i = parola.length -1; i >= 0; i--) {
+ *        reverse += parola[i];
+ *     }
+ * 
+ *     if ( reverse == parola ) {
+ *         return 'è palindroma';
+ *     }
+ * 
+ *     return 'non è palindroma';
+ * } */
+
+/************************* FINE FORMA BASE **************************/
+
+
+/********************** INIZIO FORMA MIGLIORATA *********************/
 
 // Collegamento bottone
 var buttonInviaParola = document.getElementById('button-invia-parola');
@@ -16,6 +50,8 @@ buttonInviaParola.addEventListener('click',
         var parolaUtente = document.getElementById('parola-utente').value;
         var risultato = document.getElementById('risultato');
         
+        parolaUtente = parolaUtente.trim().toLowerCase();
+
         if ( palindroma(parolaUtente) == true ) {
             risultato.innerHTML = 'La parola ' + parolaUtente + ' è palindroma.'
         } else {
@@ -40,3 +76,4 @@ function palindroma (parola) {
     return false;
 }
  
+/*********************** FINE FORMA MIGLIORATA *********************/
