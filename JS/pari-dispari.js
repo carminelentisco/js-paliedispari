31c299cf9b1c40fd247514906a3f36771839673b
carmine <carminelentisco@gmail.com>
2020-04-03 10:29:10 +0200

    JS Pari - Dispari
    Riaggiunta La versione base + Versione Migliorata

--- JS/pari-dispari.js
+++ JS/pari-dispari.js
@@ -9,6 +9,55 @@
  *
 \**************************************************************************************/
 
+/********************************* INIZIO FORMA BASE **********************************/
+
+/** // Scelte dell' utente
+ * var selectPariDispari = prompt('Inserisci Pari o Dispari : ');
+ * var selectNumber = parseInt( prompt('Inserisci un numero da 1 a 5 : '));
+ * console.log('Selezione Pari / Dispari : ' + selectPariDispari);
+ * console.log('Selezione numero utente : ' + selectNumber);
+ * 
+ * // Numero Random AI
+ * var randomNuberComputer = randomAiNumber();
+ * console.log('Numero Random AI : ' + randomNuberComputer);
+ * 
+ *  // Somma Dei numeri 
+ * var somma = selectNumber + randomNuberComputer;
+ * console.log('Somma dei numeri : ' + somma);
+ * 
+ * // Pari o Dispari 
+ * var checkPariDispari = pariDispari(somma);
+ * console.log('Il Numero sommato è : ' + checkPariDispari);
+ * 
+ * //Risultato finale
+ * 
+ * if (selectPariDispari == checkPariDispari) {
+ *     console.log('Hai Vinto');
+ * } else {
+ *     console.log('Hai Perso');
+ *     
+ * }
+ * 
+ * ////////// FUNZIONI \\\\\\\\\\
+ * 
+ * // Generatore di numeri random
+ * function randomAiNumber () {
+ *     var random = Math.floor( Math.random() * 5) + 1;
+ *     return random;
+ * }
+ * 
+ * // Check Pari-Dispari 
+ * function pariDispari(numero) {
+ *     if (numero % 2 == 0) {
+ *         return 'pari';
+ *     }
+ *     return 'dispari';
+ * } */
+
+/********************************* FINE FORMA BASE ************************************/
+
+/***************************** INIZIO FORMA MIGLIORATA ********************************/
+
 // Prendo il click bottone
 var buttonPariDispari = document.getElementById('invia-pari-dispari');
 
@@ -22,20 +71,33 @@ buttonPariDispari.addEventListener('click',
         var somma = selectNumber + parseInt(randomNumberComputer);
         var checkPariDispari = pariDispari(somma);
         
+        // Conversione parola in minuscolo + correzione degli spazi
+        selectPariDispari = selectPariDispari.trim().toLowerCase();
 
-        document.getElementById('inputText').innerHTML = selectPariDispari;
-        document.getElementById('inputNumero').innerHTML = selectNumber;
-        document.getElementById('outputRandom').innerHTML = randomNumberComputer;
-        document.getElementById('outputSomma').innerHTML = somma;
-        document.getElementById('outputSommaPariDispari').innerHTML = checkPariDispari;
+        // Controllo range numeri inseriti
         
-        var risultato = document.getElementById('risultato-pari-dispari');
-
-        if (selectPariDispari == checkPariDispari) {
-            risultato.innerHTML = "Hai Vinto";
+        if (selectNumber > 5 ){
+            alert('Errore');
         } else {
-            risultato.innerHTML = "Hai Perso";
+            
+            // Scrittura in HTML
+            document.getElementById('inputText').innerHTML = selectPariDispari;
+            document.getElementById('inputNumero').innerHTML = selectNumber;
+            document.getElementById('outputRandom').innerHTML = randomNumberComputer;
+            document.getElementById('outputSomma').innerHTML = somma;
+            document.getElementById('outputSommaPariDispari').innerHTML = checkPariDispari;
+            
+            // Dichiarazione del risultato + Scrittura in HTML
+            var risultato = document.getElementById('risultato-pari-dispari');
+
+            if (selectPariDispari == checkPariDispari) {
+                risultato.innerHTML = "Hai Vinto";
+            } else {
+                risultato.innerHTML = "Hai Perso";
+            }
         }
+
+        
 
         // Generatore di numeri random
         function randomAiNumber () {
@@ -53,5 +115,5 @@ buttonPariDispari.addEventListener('click',
     }
 );
 
-
+/******************************* FINE FORMA MIGLIORATA ********************************/
 
